2789ff13bffbe3fc29803dd0d8f01b0b782cd440
Kannappagoud-shivagond <80326156+kannappagoud-shivagond@users.noreply.github.com>
2023-12-07 11:17:30 +0530

    Update calculator.go

--- Docker-Zero-to-Hero/examples/golang-multi-stage-docker-build/calculator.go
+++ Docker-Zero-to-Hero/examples/golang-multi-stage-docker-build/calculator.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi Abhishek.Veeramalla, I am a calculator app ....")
+	fmt.Println("Hi Kannappagouda Shivakond, I am a calculator app ....")
 
 	for {
 		// Read input from the user
